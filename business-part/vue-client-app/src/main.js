e1c9619a193739ae69442a0ab651fcf5abc949f8
Tomasz Bieniek <tomek-1996-1996@o2.pl>
2018-10-28 23:25:16 +0100

    Initial Vue navbar version

--- business-part/vue-client-app/src/main.js
+++ business-part/vue-client-app/src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { Navbar, FormInput, Button } from 'bootstrap-vue/es/components'
 
+Vue.use(Navbar)
+Vue.use(FormInput)
+Vue.use(Button)
 Vue.config.productionTip = false
+
 
 new Vue({
   render: h => h(App)
