0c3798d41789b08338dc84c5a40739397b69497c
Tomasz Bieniek <tomek-1996-1996@o2.pl>
2018-11-01 00:29:44 +0100

    Removing vuetify

--- business-part/vue-client-app/src/components/Footer.vue
+++ business-part/vue-client-app/src/components/Footer.vue
@@ -1,13 +1,5 @@
 <template>
 
-    <v-footer dark height="auto" fixed>
-        <v-card class="flex" flat tile>
-
-            <v-card-actions class="grey darken-3 justify-center">
-                &copy;2018 — <strong>Industrial Database project</strong>
-            </v-card-actions>
-        </v-card>
-    </v-footer>
 </template>
 
 <script>
